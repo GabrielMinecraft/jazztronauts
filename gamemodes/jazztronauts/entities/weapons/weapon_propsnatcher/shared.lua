ab72dad420267901d8a00c4df01a7201d54359a8
Gabriel M <cdlarramore1@gmail.com>
2024-12-30 00:02:27 -0500

    Multisteal and Mass Snatch rename to Buckshot.

--- gamemodes/jazztronauts/entities/weapons/weapon_propsnatcher/shared.lua
+++ gamemodes/jazztronauts/entities/weapons/weapon_propsnatcher/shared.lua
@@ -39,6 +39,7 @@ SWEP.CloseRange				= ShortRangeDefault
 -- Tier 1 settings
 local AimConeDefault		= 3
 SWEP.AutoAimCone			= AimConeDefault
+local multistealDefault		= 1
 
 -- Tier 2 settings
 SWEP.MaxFireDelayAdd		= 1.2 -- Max delay added when snatcher just started
@@ -83,6 +84,13 @@ local snatch_range = jstore.RegisterSeries("snatch_range", 10000, 10, {
 	cat = jazzloc.Localize("jazz.weapon.snatcher"),
 	priceMultiplier = 1.5,
 })
+local snatch_multisteal = jstore.RegisterSeries("snatch_multisteal", 30000, 10, {
+	name = jazzloc.Localize("jazz.weapon.snatcher.upgrade.multisteal"),
+	desc = jazzloc.Localize("jazz.weapon.snatcher.upgrade.multisteal.desc"),
+	type = "upgrade",
+	cat = jazzloc.Localize("jazz.weapon.snatcher"),
+	priceMultiplier = 1.5,
+})
 local snatch2 = jstore.Register("snatch2", 10000, {
 	name = jazzloc.Localize("jazz.weapon.snatcher.upgrade.auto"),
 	cat = jazzloc.Localize("jazz.weapon.snatcher"),
@@ -104,11 +112,11 @@ local snatch_multi = jstore.Register("snatch_multi", 50000, {
 	type = "upgrade"
 })
 
-local snatch_masssnatch = jstore.Register("snatch_masssnatch", 100000, {
+local snatch_masssnatch = jstore.Register("snatch_masssnatch", 500000, {
 	name = jazzloc.Localize("jazz.weapon.snatcher.upgrade.masssnatch"),
 	cat = jazzloc.Localize("jazz.weapon.snatcher"),
 	desc = jazzloc.Localize("jazz.weapon.snatcher.upgrade.masssnatch.desc"),
-	requires = snatch2,
+	requires = snatch_amount,
 	type = "upgrade"
 })
 
@@ -169,6 +177,10 @@ function SWEP:SetUpgrades(overpowered)
 	local rangeLevel = overpowered and 10 or jstore.GetSeries(owner, snatch_range)
 	self.MaxRange	= LongRangeDefault + rangeLevel * 150
 	self.CloseRange = ShortRangeDefault + rangeLevel * 25
+
+	-- Steal Amount
+	local multistealLevel=overpowered and 10 or multistealDefault + jstore.GetSeries(owner,snatch_multisteal)
+	self.multistealLevel=multistealLevel
 
 	-- Tier II - Automatic fire upgrade
 	if unlocks.IsUnlocked("store", owner, snatch2) or overpowered then
@@ -492,11 +504,20 @@ function SWEP:TraceToRemove(stealWorld)
 		-- Tell the server which entity we'd like to pick
 		if self:AcceptEntity( self.ConeEnt ) then
 			self.ConeEnt.JazzSnatchWait = CurTime() + 2.0
+			local tosteal={self.ConeEnt}
+			if self.multistealLevel > 1 then
+				for i = 1,self.multistealLevel do
+					pcall(function() table.insert(tosteal,self.ConeAccept[i]) end)
+				end
+			end
+
+			for _,v in pairs(tosteal) do
 			net.Start( "remove_client_send_trace" )
 			net.WriteBit(1)
 			net.WriteEntity( self )
-			net.WriteEntity( self.ConeEnt )
+			net.WriteEntity( v )
 			net.SendToServer()
+			end
 
 			self:EmitSound( self.SnatchSounds[math.random(1,#self.SnatchSounds)], 50, math.random( 95, 105 ), 1, CHAN_AUTO )
 			
